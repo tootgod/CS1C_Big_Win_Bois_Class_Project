9ad5d6c7d37584805682b8f9d7a73d19565324a3
Kyler Geesink <32684761+tootgod@users.noreply.github.com>
2017-11-19 16:16:04 -0800

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "form.h"
+#include "shapedrawer.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+    fun->show();
+    this->hide();
+}
---
+++ shapedrawer.cpp
@@ -0,0 +1,26 @@
+#include "shapedrawer.h"
+#include "ui_shapedrawer.h"
+#include <QPainter>
+
+ShapeDrawer::ShapeDrawer(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ShapeDrawer)
+{
+    ui->setupUi(this);
+    setMaximumSize(1003,671);
+}
+
+ShapeDrawer::~ShapeDrawer()
+{
+    delete ui;
+}
+
+void ShapeDrawer::on_pushButton_clicked()
+{
+    test->show();
+}
+
+void ShapeDrawer::on_createCircle_clicked()
+{
+
+}
