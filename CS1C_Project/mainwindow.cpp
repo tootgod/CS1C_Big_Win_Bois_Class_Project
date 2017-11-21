942c2e733aa406dbd70085672f8045ecbd21557c
Kyler Geesink <32684761+tootgod@users.noreply.github.com>
2017-11-20 23:07:28 -0800

    yub

--- CS1C_Project/mainwindow.cpp
+++ CS1C_Project/mainwindow.cpp
@@ -99,7 +99,6 @@ void MainWindow::on_CreateShape_clicked()
     case 4:
         mainLayout->addWidget(renderArea);
         ui->centralWidget->setLayout(mainLayout);
-        ui->setupUi(this);
         break;
     default:
         break;
