86a3dd7e7ee97f440681555ff7c1d36d746aacb9
StephenMerwin <32685119+StephenMerwin@users.noreply.github.com>
2017-11-30 12:13:08 -0800

    template

--- CS1C_Project/vector.h
+++ CS1C_Project/vector.h
@@ -68,6 +68,7 @@ public:
 
 //sort functions
     //sort and comparison function will determine which comparison to do with function pointer
+    template <class S>
     void sort(bool (*compare)(S*, S*)){
         for (int j = 0; j < size_v - 1; ++j) {
             int min = j;
