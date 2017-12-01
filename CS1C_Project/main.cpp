1f2a735d8fda5136ed3ef230ca3b7e9599533ae6
StephenMerwin <32685119+StephenMerwin@users.noreply.github.com>
2017-11-30 23:19:08 -0800

    whitebox testing main

--- CS1C_Project/main.cpp
+++ CS1C_Project/main.cpp
@@ -4,29 +4,24 @@
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     //------------Runs QT Program----------------
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
-    return a.exec();
-}
-    //------------Do not touch-------------------
-  /*  Rectangle rec(10, 20, 1234, 200, 60);
-
+    //Vector<Shape> v(20);
+    //------------Whitebox testing-------------------
+    QPainter *qp = &a;
     cout << "--------------------------------- Testing rectangle ---------------------------------" << endl;
     cout << " " << endl;
 
+    Rectangle rec(qp, 10, 20, 1234, 200, 60);
     rec.set_id(1);
 
+    cout << "For Rectangle, calculate area " << rec.calcArea() << endl;
 
-    cout << "For Rectangle, calculate area " << endl;
-    rec.printArea(rec);
-
-    cout << "For Rectangle, calculate Perimeter " << endl;
-    rec.printPerimeter(rec);
+    cout << "For Rectangle, calculate Perimeter " << rec.calcPerimeter() << endl;
 
     cout << "Draw test" << endl;
     rec.draw();
@@ -34,48 +29,60 @@ int main(int argc, char *argv[])
     cout << "--------------------------------- Testing Square ---------------------------------" << endl;
     cout << " " << endl;
 
-    Square sqr(10, 20, 1235, 45);
+    Square sqr(qp, 10, 20, 1235, 45);
 
-    cout << "For Square, calculate area " << endl;
-    sqr.printArea(sqr);
+    cout << "For Square, calculate area " << sqr.calcArea() << endl;
 
-    cout << "For Square, calculate Perimeter " << endl;
-    sqr.printPerimeter(sqr);
-
+    cout << "For Square, calculate Perimeter " << sqr.calcPerimeter() << endl;
 
     cout << "Draw test" << endl;
     sqr.draw();
+    
+    cout << "--------------------------------- Testing Circle ---------------------------------" << endl;
+    cout << " " << endl;
+    
+    Circle crc(qp, 10, 20, 1236, 45);
+    
+    cout << "For Circle, calculate area " << crc.calcArea() << endl;
+    
+    cout << "For Circle, calculate Perimeter " << crc.calcPerimeter() << endl;
+    
+    cout << "Draw test" << endl;
+    crc.draw();
 
-
-
+    cout << "--------------------------------- Testing Ellipse ---------------------------------" << endl;
+    cout << " " << endl;
+    
+    Ellipse ell(qp, 10, 20, 1236, 45, 40);
+    
+    cout << "For Ellipse, calculate area " << ell.calcArea() << endl;
+    
+    cout << "For Ellipse, calculate Perimeter " << ell.calcPerimeter() << endl;
+    
+    cout << "Draw test" << endl;
+    ell.draw();
 
     cout << "--------------------------------- Testing Polygon ---------------------------------" << endl;
     cout << " " << endl;
 
     int x_points2[4] = { 1, 3, 3, 1 };  // area 8
     int y_points2[4] = { 1, 1, 5, 5 };  // perimeter = 2 + 2 + 4 + 4 = 12
-    Polygon polygon(44444, 4, x_points2, y_points2);
+    Polygon polygon(qp, 44444, 4, x_points2, y_points2);
 
+    cout << "For Polygon, calculate area " << polygon.calcArea() << endl;
 
-
-    cout << "For Polygon, calculate area " << endl;
-    polygon.printArea(polygon);
-
-    cout << "For Polygon, calculate Perimeter " << endl;
-    polygon.printPerimeter(polygon);
+    cout << "For Polygon, calculate Perimeter " << polygon.calcPerimeter() << endl;
 
     cout << "Draw test" << endl;
     polygon.draw();
 
     cout << "--------------------------------- Line ---------------------------------" << endl;
 
-    Line line(30, 50, 33333, 30, 70);
+    Line line(qp, 30, 50, 33333, 30, 70);
 
-    cout << "For line, calculate area " << endl;
-    line.printArea(line);
-
-    cout << "For line, calculate Perimeter " << endl;
-    line.printPerimeter(line);
+    cout << "For line, calculate area " << line.calcArea() << endl;
+    
+    cout << "For line, calculate Perimeter " << line.calcPerimeter() << endl;
 
     cout << "Draw test" << endl;
     line.draw();
@@ -84,26 +91,16 @@ int main(int argc, char *argv[])
 
     int x_points[4] = { 1, 2, 3, 4};
     int y_points[4] = { 1, 2, 3, 4};
-    Polyline polyline(1237, 4, x_points, y_points);
+    Polyline polyline(qp, 1237, 4, x_points, y_points);
 
-    cout << "For polyline, calculate area " << endl;
-    polyline.printArea(polyline);
+    cout << "For polyline, calculate area " << polyline.calcArea() << endl;
 
-    cout << "For polyline, calculate Perimeter " << endl;
-    polyline.printPerimeter(polyline);
+    cout << "For polyline, calculate Perimeter " << polyline.calcPerimeter() << endl;
 
     cout << "Draw test" << endl;
     polyline.draw();
     system("pause");
+    
+    
+    return a.exec();
 }
-
-
-
-- ellipse
-- text
-"
-Text Properties: Text String, Text Color, Text Alignment, Text
-Point Size, Text Font Family, Text Font Style, Text Font Weight
-(refer to shape_input_file_specs.txt)"(2D Graphics Modeler Class Project.pdf )
-
-*/
